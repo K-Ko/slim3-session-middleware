1d30cc2a382f22122139ae6751f04c77e4a68bfc
Knut Kohl <github@knutkohl.de>
2025-06-19 12:55:45 +0200

    Fix call order

--- src/Middleware/SessionMiddleware.php
+++ src/Middleware/SessionMiddleware.php
@@ -21,32 +21,35 @@ class SessionMiddleware
                 $this->{$key} = $value;
             }
         }
+    }
 
-        // Parse the time to live value into a Unix timestamp if it is desined
-        // as a string.
-        if ($this->lifetime && is_string($ttl = $this->lifetime)) {
-            $this->lifetime = strtotime($ttl) - time();
-        }
+    /**
+     * @param \Psr\Http\Message\ServerRequestInterface $request  PSR7 request
+     * @param \Psr\Http\Message\ResponseInterface      $response PSR7 response
+     * @param callable                                 $next     Next middleware
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next)
+    {
+        // Only once
+        if (PHP_SESSION_NONE === session_status()) {
+            // Parse the time to live value into a Unix timestamp if it is desined
+            // as a string.
+            if ($this->lifetime && is_string($ttl = $this->lifetime)) {
+                $this->lifetime = strtotime($ttl) - time();
+            }
 
-        // Set the configuration options.
-        $this->configure($this->ini_settings);
+            // If the maxlifetime is less than the ttl, update the configuration of
+            // the maxlifetime to bt the value of tll multiplied.
+            if ($this->lifetime && ini_get('session.gc_maxlifetime') < $this->lifetime) {
+                ini_set('session.gc_maxlifetime', $this->lifetime * 2);
+            }
 
-        // If the maxlifetime is less than the ttl, update the configuration of
-        // the maxlifetime to bt the value of tll multiplied.
-        if ($this->lifetime && ini_get('session.gc_maxlifetime') < $this->lifetime) {
-            $this->configure([
-                'session.gc_maxlifetime' => $this->lifetime * 2,
-            ]);
-        }
+            // Set the session cookie parameters.
+            session_set_cookie_params($this->lifetime, $this->path, $this->domain, $this->secure, $this->httponly);
 
-        // Set the session cookie parameters.
-        session_set_cookie_params($this->lifetime, $this->path, $this->domain, $this->secure, $this->httponly);
-
-        // Refresh session cookie when sessions are enabled, but none exists.
-        $sessionsEnabledNoneExists = PHP_SESSION_NONE === session_status();
-
-        if ($sessionsEnabledNoneExists && $this->autorefresh && isset($_COOKIE[$this->name])) {
-            setcookie(
+            $this->autorefresh && isset($_COOKIE[$this->name]) && setcookie(
                 $this->name,
                 $_COOKIE[$this->name],
                 $this->lifetime ? time() + $this->lifetime : 0,
@@ -55,41 +58,30 @@ class SessionMiddleware
                 $this->secure,
                 $this->httponly
             );
-        }
 
-        // Set the current session name.
-        session_name($this->name);
+            // Set the current session name.
+            session_name($this->name);
 
-        // Set the user-level session storage functions
-        if ($this->handler) {
-            if (!($this->handler instanceof \SessionHandlerInterface)) {
-                $this->handler = new $this->handler();
+            // Set the user-level session storage functions
+            if ($this->handler) {
+                if (!($this->handler instanceof \SessionHandlerInterface)) {
+                    $this->handler = new $this->handler();
+                }
+
+                session_set_save_handler($this->handler, true);
             }
-            session_set_save_handler($this->handler, true);
-        }
 
-        // Set the current cache limiter
-        session_cache_limiter(false);
+            // Set the current cache limiter
+            session_cache_limiter(false);
 
-        // Start a session if sessions are enabled, but none exist.
-        if ($sessionsEnabledNoneExists) {
             session_start();
+
             $this->autorefresh && session_regenerate_id(true);
         }
-    }
 
-    /**
-     * @param Psr\Http\Message\ServerRequestInterface $request  PSR7 request
-     * @param Psr\Http\Message\ResponseInterface      $response PSR7 response
-     * @param callable                                $next     Next middleware
-     *
-     * @return Response
-     */
-    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next)
-    {
         $response = $next($request, $response);
 
-        session_write_close();
+        PHP_SESSION_ACTIVE === session_status() && session_write_close();
 
         return $response;
     }
@@ -144,24 +136,4 @@ class SessionMiddleware
      * @var mixed
      */
     protected $lifetime = 0;
-
-    // ----------------------------------------------------------------------
-    // PRIVATE
-    // ----------------------------------------------------------------------
-
-    /**
-     * @param array $settings
-     *
-     * @return $this
-     */
-    private function configure(array $settings)
-    {
-        foreach ($settings as $key => $value) {
-            if (0 === strpos($key, 'session.')) {
-                ini_set($key, $value);
-            }
-        }
-
-        return $this;
-    }
 }
